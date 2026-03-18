265b71ad4c0b29223748e3c84d03ffd4b46cbc1a
KwakChinSuk <kjs819@gmail.com>
2026-03-19 04:20:01 +0900

    auto: 2026-03-19 04:20:01 KST

--- ga-start.sh
+++ ga-start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-APP_NAME="${1:-alog-devtest}" 
-BASE_DIR="/home/ec2-user/alog/${APP_NAME}"
 LOG_DIR="/home/ec2-user/batch/log"
 PYTHON="/usr/bin/python3"
 SENDMAIL="/home/ec2-user/bin/sendmail-file"
+
+APP_NAME="alog-bori"
+BASE_DIR="/home/ec2-user/alog/${APP_NAME}"
 
 cd "${BASE_DIR}" || exit 1
 
@@ -12,34 +13,22 @@ ${PYTHON} alog_ga_app/ga_to_db.py INIT,-2 > "${LOG_DIR}/ga-INIT-${APP_NAME}.log"
 
 ${PYTHON} alog_ga_app/ga_to_db.py -2 Y  > "${LOG_DIR}/ga-${APP_NAME}.log" 2>&1
 
-## 로그 확인 방법
-##${PYTHON} alog_ga_app/ga_to_db.py -2 Y
-
 chown -R ec2-user:ec2-user "${BASE_DIR}"
-
-sleep 1
-#${SENDMAIL} ga-complete[${APP_NAME}] ${LOG_DIR}/ga-${APP_NAME}.log
-
-case "${APP_NAME}" in
-    "alog-half")
-        TARGET_URL="https://ga.halfclub.com"
-        ;;
-    "alog-bori")
-        TARGET_URL="https://ga.boribori.co.kr"
-        ;;
-    "alog-test")
-        TARGET_URL="http://alog-test.alog.ai.kr"
-        ;;
-    *)
-        echo "Error: Defined APP_NAME not found (Current: ${APP_NAME})"
-        ;;
-esac
-
-#sleep 1
-# 설정된 ${TARGET_URL} 변수를 사용하여 curl 요청
-#/usr/bin/curl -f -s -S "${TARGET_URL}?pbatchcode=alog-api@cskwak-152504.iam.gserviceaccount.com" > /home/ec2-user/batch/log-www-check/ga-complete-${APP_NAME}.log 2>&1
 
 sleep 1
 ${SENDMAIL} [${APP_NAME}]ga-complete ${LOG_DIR}/ga-${APP_NAME}.log
 
+APP_NAME="alog-half"
+BASE_DIR="/home/ec2-user/alog/${APP_NAME}"
+
+cd "${BASE_DIR}" || exit 1
+
+${PYTHON} alog_ga_app/ga_to_db.py INIT,-2 > "${LOG_DIR}/ga-INIT-${APP_NAME}.log" 2>&1
+
+${PYTHON} alog_ga_app/ga_to_db.py -2 Y  > "${LOG_DIR}/ga-${APP_NAME}.log" 2>&1
+
+chown -R ec2-user:ec2-user "${BASE_DIR}"
+
+sleep 1
+${SENDMAIL} [${APP_NAME}]ga-complete ${LOG_DIR}/ga-${APP_NAME}.log
 
