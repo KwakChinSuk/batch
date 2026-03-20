0aab4fb10144af3dc434842398d12ad424302b85
KwakChinSuk <kjs819@gmail.com>
2026-03-21 04:20:01 +0900

    auto: 2026-03-21 04:20:01 KST

--- ga-start.sh
+++ ga-start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#APP_NAME="${1:-alog-devtest}" 
+APP_NAME="${1:-alog-devtest}" 
 LOG_DIR="/home/ec2-user/batch/log"
 PYTHON="/usr/bin/python3"
 SENDMAIL="/home/ec2-user/bin/sendmail-file"
 
 
-APP_NAME="alog-bori"
+#APP_NAME="alog-bori"
 BASE_DIR="/home/ec2-user/alog/${APP_NAME}"
 
 cd "${BASE_DIR}" || exit 1
@@ -20,17 +20,4 @@ chown -R ec2-user:ec2-user "${BASE_DIR}"
 sleep 1
 ${SENDMAIL} [${APP_NAME}]ga-complete ${LOG_DIR}/ga-${APP_NAME}.log
 
-APP_NAME="alog-half"
-BASE_DIR="/home/ec2-user/alog/${APP_NAME}"
-
-cd "${BASE_DIR}" || exit 1
-
-${PYTHON} alog_ga_app/ga_to_db.py INIT,-2 > "${LOG_DIR}/ga-INIT-${APP_NAME}.log" 2>&1
-
-${PYTHON} alog_ga_app/ga_to_db.py -2 Y  > "${LOG_DIR}/ga-${APP_NAME}.log" 2>&1
-
-chown -R ec2-user:ec2-user "${BASE_DIR}"
-
-sleep 1
-${SENDMAIL} [${APP_NAME}]ga-complete ${LOG_DIR}/ga-${APP_NAME}.log
 
